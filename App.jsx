161b850a47d89811ffb70426f0d9844c57c81115
Makc082 <sawivanko@gmail.com>
2025-09-07 16:14:21 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,32 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div className="container">
+
+            <h1 className="header">Кратка інформація</h1>
+
+            <img
+                className="profile-photo"
+                src="https://st4.depositphotos.com/16233166/38900/i/450/depositphotos_389008128-stock-photo-confident-happy-young-man-checkered.jpg"
+                alt="Profile"
+            />
+
+
+            <div className="info">
+                <div>Full name: Мандрика Максим Сергійович</div>
+                <div>Date of birth: 14.02.1982</div>
+                <div>Age: 43</div>
+                <div>Phone: +380508438986</div>
+                <div>Email: sawivanko@gmail.com</div>
+            </div>
+        </div>
+
+    </>
+  )
+}
+
+export default App
